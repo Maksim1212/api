84d94d7f4b26ca9cf20e164a75f0dc55dcd8847f
Maksim1212 <mr.frost201611@gmail.com>
2019-02-13 12:40:17 +0200

    Created Model -a

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Model\Car::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('/cars','CarController');
+
+//
+Route::group(['prefix'=>'products'],function() {
+    Route::apiResource('/{product}/review', 'ReviewController');
+});
+
