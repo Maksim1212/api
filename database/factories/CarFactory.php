e94312ae45c93a473b94a04373a733a800d29f35
Maksim1212 <mr.frost201611@gmail.com>
2019-02-13 13:48:33 +0200

    Seeding part for Product and Review

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -4,6 +4,15 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Car::class, function (Faker $faker) {
     return [
-        //
+
+        'name' => $faker->word,
+        'condition' => $faker->word,
+        'age' => $faker->numberBetween(1,100),
+        'free' => $faker->word,
+        'start_route_at' => $faker->dateTime($max = 'now', $timezone = null),
+        'finish_route_at' => $faker->dateTime($max = 'now', $timezone = null),
+        'start_repairs_at' => $faker->dateTime($max = 'now', $timezone = null),
+        'finish_repairs_at' => $faker->dateTime($max = 'now', $timezone = null)
+
     ];
 });
