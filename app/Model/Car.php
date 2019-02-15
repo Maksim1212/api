3b3ea22cafff6988c8ad20370783ef9a5ee36db3
Maksim1212 <mr.frost201611@gmail.com>
2019-02-15 10:17:39 +0200

    Passport installed

--- app/Model/Car.php
+++ app/Model/Car.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model
 {
-    protected $fillable = ['name', 'age','free','condition','start_route_at','finish_route_at','start_repairs_at','finish_repairs_at'];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+//    protected $fillable = ['name', 'age','free','condition','start_route_at','finish_route_at','start_repairs_at','finish_repairs_at'];
+//
+//    public function user()
+//    {
+//        return $this->belongsTo(User::class);
+//    }
 }
