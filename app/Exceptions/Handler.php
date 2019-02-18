409c83b70bb6497c8356a036e0d16049e377dfae
Maksim1212 <mr.frost201611@gmail.com>
2019-02-18 13:45:58 +0200

    Customised Exception Trait

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,13 +2,14 @@
 
 namespace App\Exceptions;
 
+use App\Exceptions\ExceptionTrait;
 use Exception;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Nexmo\Response;
+
 
 class Handler extends ExceptionHandler
 {
+    use ExceptionTrait;
     /**
      * A list of the exception types that are not reported.
      *
@@ -31,6 +32,8 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
      * @param  \Exception  $exception
      * @return void
      */
@@ -48,27 +51,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if($request->expectsJson()) {
-
-            if ($exception instanceof ModelNotFoundException) {
-
-                return response()->json([
-                    'errors' => 'Car Model not found'
-                ], Response::HTTP_NOT_FOUND);
-            }
-
-
-            if ($exception instanceof NotFoundException) {
-
-                return response()->json([
-                    'errors' => 'Incorect route'
-                ], Response::HTTP_NOT_FOUND);
-            }
-
+        if ($request->expectsJson()) {
+            return $this->apiException($request,$exception);
         }
-
-
-
         return parent::render($request, $exception);
     }
 }
