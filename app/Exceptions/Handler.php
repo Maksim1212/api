766b14d27f86b9364439956b0313f04469bf5f5d
Maksim1212 <mr.frost201611@gmail.com>
2019-02-18 13:26:34 +0200

    Handles the exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,9 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Nexmo\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +48,27 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($request->expectsJson()) {
+
+            if ($exception instanceof ModelNotFoundException) {
+
+                return response()->json([
+                    'errors' => 'Car Model not found'
+                ], Response::HTTP_NOT_FOUND);
+            }
+
+
+            if ($exception instanceof NotFoundException) {
+
+                return response()->json([
+                    'errors' => 'Incorect route'
+                ], Response::HTTP_NOT_FOUND);
+            }
+
+        }
+
+
+
         return parent::render($request, $exception);
     }
 }
